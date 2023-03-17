ff3dd21d648fd25b1fb76eb008f578d56669424e
cburnjohnson <cburnjohnson@gmail.com>
2023-03-16 21:13:50 -0700

    some setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  fooder
+  <Navbar />
+  <Hero />
 </template>
 
 <script setup lang="ts">
-
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
 </script>
 
 <style scoped>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    Fooder
+  </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
