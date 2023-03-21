838214ac7a22004e7baf12a96e8a4cd36cb82f50
cburnjohnson <cburnjohnson@gmail.com>
2023-03-20 19:51:43 -0700

    setup

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,17 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
+import axios from 'axios';
+
+async function getRestaurants() {
+  const data = await axios.get('')
+
+  console.log(data); //eslint-disable-line
+}
+
+getRestaurants();
+
+
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
